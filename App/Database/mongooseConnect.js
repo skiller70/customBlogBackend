3410bd008fe1c11b87463fc7630e445c22967f14
skiller70 <singhlucky007007@gmail.com>
2022-08-24 12:34:06 +0530

    edit config

--- App/Database/mongooseConnect.js
+++ App/Database/mongooseConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const LOCAL_URL = "mongodb://localhost:27017"
+// const LOCAL_URL = "mongodb://localhost:27017"
 const URL = "mongodb+srv://skiller007:iePhnsJRts0QgrIP@cluster0.jsjeb.mongodb.net/customBlog?retryWrites=true&w=majority"
 mongoose.connect(process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
