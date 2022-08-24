2c728201d9b1e1af20bd4395b29e989f9f48a996
skiller70 <singhlucky007007@gmail.com>
2022-08-24 12:26:44 +0530

    fix: refactor hard coded connection string to Heroku config var

--- App/Database/mongooseConnect.js
+++ App/Database/mongooseConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const LOCAL_URL = "mongodb://localhost:27017"
 const URL = "mongodb+srv://skiller007:iePhnsJRts0QgrIP@cluster0.jsjeb.mongodb.net/customBlog?retryWrites=true&w=majority"
-mongoose.connect(process.env.MONGOD,
+mongoose.connect(process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
    ).then((data)=> console.log("connected to the database")).catch((err)=>console.log(err))
 
