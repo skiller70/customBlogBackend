4461b29bbf03b1bb730c68a059d9070072b10dd8
skiller70 <singhlucky007007@gmail.com>
2022-08-24 12:20:23 +0530

    fix connection issue

--- App/Database/mongooseConnect.js
+++ App/Database/mongooseConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const LOCAL_URL = "mongodb://localhost:27017"
 const URL = "mongodb+srv://skiller007:iePhnsJRts0QgrIP@cluster0.jsjeb.mongodb.net/customBlog?retryWrites=true&w=majority"
-mongoose.connect(URL,
+mongoose.connect(process.env.MONGOD,
     { useNewUrlParser: true, useUnifiedTopology: true }
    ).then((data)=> console.log("connected to the database")).catch((err)=>console.log(err))
 
