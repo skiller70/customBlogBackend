d197d19e81413ccf1e235a246601cec5648b433b
skiller70 <singhlucky007007@gmail.com>
2022-09-02 20:44:11 +0530

    fixing db

--- App/Database/mongooseConnect.js
+++ App/Database/mongooseConnect.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
 
-
+// const LOCAL_URL = "mongodb://localhost:27017"
 mongoose.connect(process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
    ).then((data)=> console.log("connected to the database")).catch((err)=>console.log(err))
