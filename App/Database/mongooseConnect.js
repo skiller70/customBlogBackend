3852685373205a09f4a211083545c74b6cedb6c6
skiller70 <singhlucky007007@gmail.com>
2022-09-02 13:34:15 +0530

    fixing db error

--- App/Database/mongooseConnect.js
+++ App/Database/mongooseConnect.js
@@ -1,9 +1,8 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
 
-const LOCAL_URL = "mongodb://localhost:27017"
-const URL = "mongodb+srv://skiller007:iePhnsJRts0QgrIP@cluster0.jsjeb.mongodb.net/customBlog?retryWrites=true&w=majority"
-mongoose.connect(LOCAL_URL,
+
+mongoose.connect(process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
    ).then((data)=> console.log("connected to the database")).catch((err)=>console.log(err))
 
