70a3845fdd59d6360b0e1b6d3b39ecb498dc9950
skiller70 <singhlucky007007@gmail.com>
2022-09-03 08:40:56 +0530

    Added origin netlify

--- App/Routes/router.js
+++ App/Routes/router.js
@@ -9,22 +9,14 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(
   cors({
-    origin: "https://luckyskiller.herokuapp.com/blog",
-    
+    origin: 'https://main--remarkable-macaron-2714af.netlify.app/',
+    credentials: true,
+  
   })
+
+
+  
 );
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
-
-
-
-
 
 
 
