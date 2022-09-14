568f6258e6eab40f6225853f1cd9f46a9f7f4b32
skiller70 <singhlucky007007@gmail.com>
2022-09-14 17:48:35 +0530

    infinite scroll

--- App/controller/getBlogs.js
+++ App/controller/getBlogs.js
@@ -1,9 +1,18 @@
 const mongoModel = require("../Database/mongooseSchema");
 
 module.exports = async (req, res) => {
+  const limit = req.query.limit;
+  const page = (req.query.page - 1) * limit;
   try {
-    const postData = await mongoModel.BLOG_POST.find({}).populate("author","username").sort({_id: -1});
-   return res.status(200).send(postData)
+    const allDataLength = await mongoModel.BLOG_POST.find({});
+    const lastPage = Math.ceil(allDataLength.length / 3);
+    const postData = await mongoModel.BLOG_POST.find({})
+      .limit(limit)
+      .skip(page)
+      .populate("author", "username")
+      .sort({ _id: -1 });
+
+    return res.status(200).json({ data: postData, lastPage });
   } catch (error) {
     if (error) return res.send("failed to fetch");
   }
