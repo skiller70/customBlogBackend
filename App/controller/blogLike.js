366eca2a2455c11c8feae3a85ffec64c3d6a2abb
skiller70 <singhlucky007007@gmail.com>
2022-09-20 19:21:30 +0530

    added likes

--- App/controller/blogLike.js
+++ App/controller/blogLike.js
@@ -1,15 +1,31 @@
-const mongoModel = require("../Database/mongooseSchema")
+const mongoModel = require("../Database/mongooseSchema");
 
-module.exports = async(req,res)=>{
-   
+module.exports = async (req, res) => {
+    const  { postId, author,remove} = await req.query;
+  try {
 
-try {
-    const {blogId,author} = req.query;
-    await mongoModel.BLOG_POST.updateOne({_id :blogId},{$push : { likes : {$each : [author]}}})
-    res.status(201).send("like successful")
-} catch (error) {
+
+if(remove === "true"){
+    await mongoModel.BLOG_POST.findByIdAndUpdate(
+        { _id: postId },    
+        { $pull: { "likes" : author  } }
+      );
+     return res.status(200).send("like remove successfully");
+
+}
+else{
+    await mongoModel.BLOG_POST.findByIdAndUpdate(
+        { _id: postId },
+        { $push: { likes: { $each: [author] } } }
+      );
+     return res.status(200).send("like successful");
     
-    if(error){return res.status(400).send("failed to like post")}
 }
 
-}
+
+  } catch (error) {
+    if (error) {
+      return res.status(400).send("failed to like post");
+    }
+  }
+};
